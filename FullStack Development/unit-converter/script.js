c24f9141f31e571718c1bdba04c4ae6867e6f9fe
Akinsola Oluwatomisin Oswald <akinsolaoluwatomisin475@gmail.com>
2026-01-08 20:51:57 -0200

    modified file

---
+++ FullStack Development/unit-converter/script.js
@@ -0,0 +1,18 @@
+const numEl = document.getElementById("num-el")
+const convertBtn = document.getElementById("convert-el")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById(mass-el)
+let value
+
+convertBtn.addEventListener("click", function () { 
+
+
+
+})
+
+function lengthCon() {
+
+
+
+}
