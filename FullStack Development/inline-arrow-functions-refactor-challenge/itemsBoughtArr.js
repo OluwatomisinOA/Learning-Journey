37f96485526736578832cf513a7c3f1c37f0e12d
Akinsola Oluwatomisin Oswald <akinsolaoluwatomisin475@gmail.com>
2026-01-10 22:45:03 -0200

    added a file in the working directory

---
+++ FullStack Development/inline-arrow-functions-refactor-challenge/itemsBoughtArr.js
@@ -0,0 +1,25 @@
+export const itemsBoughtArr = [
+  {
+    name: 'Electrical Toothbrush Holder',
+    priceUSD: 40,
+    desc: 'A robotic arm that holds and moves yoyr electric toothbrush for you, ensuring optimal brushing technique.'
+  },
+
+  {
+    name: 'Inviible Milk',
+    priceUSD: 10,
+    desc: 'A carbon of milk that turns completely transparent when poured into a glass, providing a magical and mysterious drinking experience.'
+  },
+
+  {
+    name: 'Self-Chilling Soup Can',
+    priceUSD: 15,
+    desc: 'A can of soup that instantly chills itself when opened, so you can enjoy a refreshing cold soup on a hot summer day.'
+  },
+
+  {
+    name: 'Glow-in-the-Dark Eggs',
+    priceUSD: 8,
+    desc: 'A corton of eggs that glow in the dark, making breakfast preparation an exciting and illuminous experience.'
+  }
+]
---
+++ FullStack Development/rest-parameter/script.js
@@ -0,0 +1,7 @@
+function setPermissionLevel(permissionLevel, ...names) {
+
+  names.forEach((name) => console.log(`${name} now has ${permissionLevel} access.`));
+
+}
+
+setPermissionLevel('admin', 'Oswald', 'JesusStar', 'Star')
