c47c1650b0784d0fa5ecb3365d63897470f62d9a
Akinsola Oluwatomisin Oswald <akinsolaoluwatomisin475@gmail.com>
2026-01-13 17:22:37 -0200

    updated working directory

---
+++ FullStack Development/Async-JavaScript/http-request-fetch/script.js
@@ -0,0 +1,34 @@
+// async and await
+
+const getTodos = async () => {
+
+  const response = await fetch('todos/utodos.json');
+
+  if (response.status !== 200) {
+    throw new Error('cannot fetch the data');
+  }
+
+  const data = await response.json();
+  return data;
+
+};
+
+
+
+getTodos()
+  .then(data => console.log('resolved:', data))
+  .catch(err => console.log('rejected:', err.message));
+
+
+
+
+// fetch api
+
+// fetch('todos/todos.json').then(response => {
+//   console.log('resolved:', response);
+//   return response.json();
+// }).then(data => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log('rejected:', err)
+// })
