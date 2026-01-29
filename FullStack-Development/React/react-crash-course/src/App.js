9d9dcbb70d171fdfa312a9ba6c743347013a2c39
Akinsola Oluwatomisin Oswald <akinsolaoluwatomisin475@gmail.com>
2026-01-29 20:34:09 +0100

    Update working directory

--- FullStack-Development/React/react-crash-course/src/App.js
+++ FullStack-Development/React/react-crash-course/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
+function ProductCard() {
+  return (
+    <article>
+      <h2>iPhone 15 Pro</h2>
+      <img
+        src="images/iphone.png"
+        alt="iPhone 15 Pro"
+        width="120px"
+        height="120px"
+      />
+      <p>Specifications:</p>
+      <ul>
+        <li>A17 Pro chip with 6-core GPU</li>
+        <li>3x or 5x Telephoto camera</li>
+        <li>Up to 29 hours video playback</li>
+      </ul>
+      <button>But (From $999)</button>
+    </article>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      React Crash Course
+      <ProductCard></ProductCard>
     </div>
   );
 }
